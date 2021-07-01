865a5c704bb328272ef44c6fad4505170c0d33aa
Antonio Massa <antonio.massa@zehus.it>
2021-07-01 10:21:51 +0200

    Modified text attributes and app accent color

--- LegoBlocks/ContentView.swift
+++ LegoBlocks/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+        TabView(selection: .constant(1)) {
             NavigationView {
-                Text("Tab Content 1")
+                Text("Text")
+                    .font(.title2)
+                    .bold()
                     .navigationTitle("News")
             }
             .tabItem { Text("News") }.tag(1)
@@ -21,13 +23,14 @@ struct ContentView: View {
                     .navigationTitle("Chat")
             }
             .tabItem { Text("Products") }.tag(2)
-           
+            
             NavigationView {
                 Text("Tab Content 3")
                     .navigationTitle("Chat")
             }
             .tabItem { Text("Chat") }.tag(3)
         }
+        .accentColor(.red)
     }
 }
 
