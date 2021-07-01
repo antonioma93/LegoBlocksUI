cae209fbacc30e76fd02c4caabeb2ab83d36db83
Antonio Massa <antonio.massa@zehus.it>
2021-07-01 10:30:48 +0200

    Added list with navigation links

--- LegoBlocks/ContentView.swift
+++ LegoBlocks/ContentView.swift
@@ -11,10 +11,14 @@ struct ContentView: View {
     var body: some View {
         TabView(selection: .constant(1)) {
             NavigationView {
-                Text("Text")
-                    .font(.title2)
-                    .bold()
-                    .navigationTitle("News")
+                List(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                    NavigationLink(destination: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Destination@*/Text("Destination")/*@END_MENU_TOKEN@*/) {
+                        Text("Text")
+                             .font(.title2)
+                             .bold()
+                    }
+                }
+                .navigationTitle("News")
             }
             .tabItem { Text("News") }.tag(1)
             
